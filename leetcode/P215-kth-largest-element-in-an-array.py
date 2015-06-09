a26492e4c09958dfc41a2df0942b0db4a9672cc7
darknight <illuminating.me@gmail.com>
2015-06-09 21:19:57 +0800

    solved p215,p217 of leetcode

---
+++ leetcode/P215-kth-largest-element-in-an-array.py
@@ -0,0 +1,14 @@
+class Solution:
+    # @param {integer[]} nums
+    # @param {integer} k
+    # @return {integer}
+    def findKthLargest(self, nums, k):
+        if len(nums) == 1:
+            return nums[0]
+        nums.sort(reverse=True)
+        return nums[k-1]
+
+
+if __name__ == '__main__':
+    print Solution().findKthLargest([3,2,1,5,6,4,6], 2)
+    print Solution().findKthLargest([-1,-1], 2)
---
+++ leetcode/P217-contains-duplicate.py
@@ -0,0 +1,15 @@
+class Solution:
+    # @param {integer[]} nums
+    # @return {boolean}
+    def containsDuplicate(self, nums):
+        if len(nums) <= 1:
+            return False
+        nums.sort()
+        for i in range(len(nums)-1):
+            if nums[i] ^ nums[i+1] == 0:
+                return True
+        return False
+
+if __name__ == '__main__':
+    print Solution().containsDuplicate([5,6,7,2,3,4,8])
+    print Solution().containsDuplicate([5,5])
