9072ff7f0b499091cdbe20086bb6e52f5f57dd09
darknight <illuminating.me@gmail.com>
2015-07-19 21:11:33 +0800

    solved p125 of leetcode

---
+++ leetcode/P125-valid-palindrome.py
@@ -0,0 +1,26 @@
+class Solution:
+    # @param {string} s
+    # @return {boolean}
+    def isPalindrome(self, s):
+        s = s.lower()
+        if len(s) < 2:
+            return True
+        i = 0
+        j = len(s) - 1
+        while i < j:
+            if not s[i].isalnum():
+                i += 1
+                continue
+            if not s[j].isalnum():
+                j -= 1
+                continue
+            if s[i] == s[j]:
+                i += 1
+                j -= 1
+            else:
+                return False
+        return True
+
+if __name__ == '__main__':
+    print Solution().isPalindrome("A man, a plan, a canal: Panama")
+    print Solution().isPalindrome("race a car")
