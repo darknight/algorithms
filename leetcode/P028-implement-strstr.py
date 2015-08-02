91fd84bb6b9370bf04ea7c000c868a8172a52716
darknight <illuminating.me@gmail.com>
2015-08-02 16:26:15 +0800

    solved p28 of leetcode

---
+++ leetcode/P028-implement-strstr.py
@@ -0,0 +1,23 @@
+class Solution:
+    # @param {string} haystack
+    # @param {string} needle
+    # @return {integer}
+    def _strStr(self, haystack, needle):
+        return haystack.find(needle)
+
+    def strStr(self, haystack, needle):
+        if len(needle) > len(haystack):
+            return -1
+        if needle == "":
+            return 0
+        step = len(needle)
+        for i in range(len(haystack) - step + 1):
+            temp_str = haystack[i:i+step]
+            if temp_str == needle:
+                return i
+        return -1
+
+if __name__ == '__main__':
+    print Solution().strStr("", "")
+    print Solution().strStr("a", "a")
+
