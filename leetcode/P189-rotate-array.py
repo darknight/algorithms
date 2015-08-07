dbc186993007c5369499f4bc8deb788c6efbadd1
darknight <illuminating.me@gmail.com>
2015-08-07 20:02:45 +0800

    solved p189 of leetcode

---
+++ leetcode/P189-rotate-array.py
@@ -0,0 +1,23 @@
+class Solution:
+    # @param {integer[]} nums
+    # @param {integer} k
+    # @return {void} Do not return anything, modify nums in-place instead.
+    def rotate(self, nums, k):
+        if len(nums) == 0:
+            return
+        for i in range(k):
+            x = nums.pop()
+            nums.insert(0, x)
+
+    # TODO
+    def rotate2(self, nums, k):
+        pass
+
+    # TODO
+    def rotate3(self, nums, k):
+        pass
+
+if __name__ == '__main__':
+    x = [1,2]
+    Solution().rotate(x, 1)
+    print x
