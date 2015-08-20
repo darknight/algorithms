50605c054734e78cb0a801b3889222d651667a3d
darknight <illuminating.me@gmail.com>
2015-08-20 21:24:58 +0800

    solved p231 of leetcode

---
+++ leetcode/P231-power-of-two.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n < 1:
+            return False
+        while n > 1:
+            if n & 0x1 == 1:
+                return False
+            n = n >> 0x1
+        return True
+
+if __name__ == '__main__':
+    print Solution().isPowerOfTwo(63)
+    print Solution().isPowerOfTwo(64)
