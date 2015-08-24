b2b5debb5c68236390a079ff9bab476dee4aa38f
darknight <illuminating.me@gmail.com>
2015-08-24 21:40:59 +0800

    solved p268 of leetcode

---
+++ leetcode/P268-missing-number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        total = sum(nums)
+        length = len(nums)
+        real_sum = (1 + length) * length / 2
+        res = real_sum - total
+        return res
+
+if __name__ == '__main__':
+    print Solution().missingNumber([0,1,3])
