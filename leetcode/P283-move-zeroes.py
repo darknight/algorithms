d863ad7b6c2bf7721d8feae2acd29d78691387bb
darknight <illuminating.me@gmail.com>
2016-02-21 20:51:48 +0800

    solved p283 of leetcode

---
+++ leetcode/P283-move-zeroes.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        size = len(nums)
+        n = 0
+        for i in range(size):
+            if nums[i] == 0:
+                n += 1
+            else:
+                nums[i-n] = nums[i]
+        for i in range(size-n, size):
+            nums[i] = 0
+
+if __name__ == '__main__':
+    Solution().moveZeroes([0,1,0,3,12])
+    Solution().moveZeroes([0,0,1])
+
