a1363bd590f9c4ed38f0f998ae66f4b35f3e57fd
darknight <illuminating.me@gmail.com>
2016-02-21 16:55:22 +0800

    solved p216 of leetcode

---
+++ leetcode/P216-combination-sum-iii.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def combinationSum3(self, k, n):
+        """
+        :type k: int
+        :type n: int
+        :rtype: List[List[int]]
+        """
+        if k > n:
+            return []
+        res = []
+        tmp = [0] * k
+
+        def _combine(i, base, num):
+            if i == k:
+                if num == 0:
+                    res.append(tmp[:])
+                return
+            for s in range(base, 10):
+                tmp[i] = s
+                _combine(i+1, s+1, num-s)
+
+        _combine(0, 1, n)
+        return res
+
+if __name__ == '__main__':
+    print Solution().combinationSum3(3, 7)
+    print Solution().combinationSum3(3, 9)
+    print Solution().combinationSum3(2, 18)
