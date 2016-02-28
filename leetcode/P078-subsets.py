a155b0e38c2dcc1b16a2823f75b055e3c9243e9f
darknight <illuminating.me@gmail.com>
2016-02-28 17:08:06 +0800

    solved p78,p86 of leetcode

---
+++ leetcode/P078-subsets.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        length = len(nums)
+        if length == 0:
+            return [[]]
+        res = []
+        nums.sort()
+        mask = [1<<i for i in range(length-1, -1, -1)]
+        for i in range(2 ** length):
+            tmp = []
+            for j, m in enumerate(mask):
+                if m & i:
+                    tmp.append(nums[j])
+            res.append(tmp)
+        return res
+
+if __name__ == '__main__':
+    print Solution().subsets([])
+    print Solution().subsets([1])
+    print Solution().subsets([1,2])
+    print Solution().subsets([3,2,1])
+    print Solution().subsets([1,3,2,4])
---
+++ leetcode/P086-partition-list.py
@@ -0,0 +1,58 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def partition(self, head, x):
+        """
+        :type head: ListNode
+        :type x: int
+        :rtype: ListNode
+        """
+        if head is None:
+            return None
+        if head and head.next is None:
+            return head
+        left_head = None
+        left_tail = None
+        right_head = None
+        right_tail = None
+        p = head
+        while p:
+            q = p.next
+            if p.val < x:
+                if left_head is None:
+                    left_head = left_tail = p
+                else:
+                    left_tail.next = p
+                    left_tail = p
+                left_tail.next = None
+            else:
+                if right_head is None:
+                    right_head = right_tail = p
+                else:
+                    right_tail.next = p
+                    right_tail = p
+                right_tail.next = None
+            p = q
+
+        if left_tail is None:
+            return right_head
+        else:
+            left_tail.next = right_head
+            return left_head
+
+if __name__ == '__main__':
+    nodes = [ListNode(i) for i in range(5)]
+    #nodes[0].next = nodes[3]
+    #nodes[3].next = nodes[2]
+    #nodes[2].next = nodes[1]
+    #nodes[1].next = nodes[4]
+    nodes[2].next = nodes[1]
+    h = Solution().partition(nodes[2], 1)
+    while h:
+        print h.val
+        h = h.next
+
