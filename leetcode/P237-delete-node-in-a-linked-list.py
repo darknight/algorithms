d40c6b3c2dc9f62f4e50e23a01b5e1c82705eb62
darknight <illuminating.me@gmail.com>
2016-03-20 14:46:22 +0800

    solved p237 of leetcode

---
+++ leetcode/P237-delete-node-in-a-linked-list.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        if node is None:
+            return
+        curr = node.next
+        while curr.next:
+            node.val = curr.val
+            node = curr
+            curr = curr.next
+        node.val = curr.val
+        node.next = None
+        del curr
