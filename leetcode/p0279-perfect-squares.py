70b6d1c2e29615adbf7d31df32dc067519d9e7da
darknight <illuminating.me@gmail.com>
2019-05-23 23:06:10 +0800

    [search] solved p279 of leetcode

--- leetcode/p0279-perfect-squares.py
+++ leetcode/p0279-perfect-squares.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 class Solution(object):
-    def numSquares(self, n):
+    def numSquares1(self, n):
         """
         :type n: int
         :rtype: int
         """
-        #TODO: too slow, improve it!
+        # slow solution
         data = []
         for i in range(1, n+1):
             x = i * i
@@ -16,30 +16,75 @@ class Solution(object):
                 break
         data.sort(reverse=True)
         res = [n]
-        tmp = []
         def _partial(part, num, params):
             for i, d in enumerate(params):
                 if part < d:
                     continue
                 if part == d:
-                    #tmp.append(d)
-                    #print('bingo=>',tmp)
-                    #tmp.pop()
-                    res[0] = min(num+1, res[0]) 
+                    res[0] = min(num+1, res[0])
                     break
                 if num+1 >= res[0]:
                     break
-                #tmp.append(d)
                 _partial(part-d, num+1, params[i:len(params)+1])
-                #tmp.pop()
         _partial(n, 0, data)
         return res[0]
 
+    #
+    # TLE
+    #
+    def numSquares2(self, n: int) -> int:
+        if n <= 3:
+            return n
+        squares = {1}
+        i = 2
+        while i * i <= n:
+            squares.add(i * i)
+            i += 1
+        queue = [n]
+        visited = set()
+        res = 0
+        while len(queue) > 0:
+            res += 1
+            tmp_queue = []
+            for data in queue:
+                if data in squares:
+                    return res
+                visited.add(data)
+                for i in range(1, data):
+                    if i not in visited and (data - i) in squares:
+                        tmp_queue.append(i)
+            queue = tmp_queue
+
+    def numSquares3(self, n: int) -> int:
+        if n <= 3:
+            return n
+        squares = {1}
+        i = 2
+        while i * i <= n:
+            squares.add(i * i)
+            i += 1
+        queue = [n]
+        visited = set()
+        res = 0
+        while len(queue) > 0:
+            res += 1
+            tmp_queue = []
+            for data in queue:
+                if data in squares:
+                    return res
+                visited.add(data)
+                for sq in squares:  # speed up significantly
+                    child = data - sq
+                    if child > 0 and child not in visited:
+                        tmp_queue.append(child)
+            queue = tmp_queue
+
+    # TODO: DP
+    def numSquares(self, n: int) -> int:
+        pass
+
 if __name__ == '__main__':
-    #print(Solution().numSquares(12))
-    #print(Solution().numSquares(13))
-    #print(Solution().numSquares(67))
-    #print(Solution().numSquares(64))
-    #print(Solution().numSquares(643))
-    #print(Solution().numSquares(7168))
-    print(Solution().numSquares(9975))
+    assert Solution().numSquares(12) == 3
+    assert Solution().numSquares(13) == 2
+    assert Solution().numSquares(67) == 3
+    assert Solution().numSquares(64) == 1
