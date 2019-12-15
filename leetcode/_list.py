7708664c189b55dceaa886cd8b0d8cfb86ee711c
darknight <illuminating.me@gmail.com>
2019-12-15 21:27:34 +0700

    update template

---
+++ leetcode/_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+if __name__ == '__main__':
+    pass
--- leetcode/_template.py
+++ leetcode/_template.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 
 import math
+import itertools
 from collections import defaultdict
 from typing import List
 from typing import Set
 try:
     from _tree import *
+except ImportError:
+    pass
+
+try:
+    from _list import *
+except ImportError:
+    pass
+
+try:
+    from _uitl import *
 except ImportError:
     pass
 
