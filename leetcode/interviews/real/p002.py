eef059bb27d80de9ba642e53fc6fe617e6e2e797
darknight <illuminating.me@gmail.com>
2019-12-26 11:15:51 +0700

    add interview questions

---
+++ leetcode/interviews/real/p002.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import math
+import itertools
+from collections import defaultdict
+from typing import List
+from typing import Set
+try:
+    from _tree import *
+except ImportError:
+    pass
+
+try:
+    from _list import *
+except ImportError:
+    pass
+
+try:
+    from _uitl import *
+except ImportError:
+    pass
+
+
+class Solution:
+    """
+    find the minimum index of target from a sorted list of integers in O(nlogn) time
+    """
+    pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ leetcode/interviews/real/p003.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import math
+import itertools
+from collections import defaultdict
+from typing import List
+from typing import Set
+try:
+    from _tree import *
+except ImportError:
+    pass
+
+try:
+    from _list import *
+except ImportError:
+    pass
+
+try:
+    from _uitl import *
+except ImportError:
+    pass
+
+
+class Solution:
+    """
+    2 * N matrix, tiled by 2 * 1 tile, how many ways?
+    for example: 2 * 4 => 5
+    """
+    pass
+
+
+if __name__ == '__main__':
+    pass
