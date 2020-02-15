e3dbc01f0fbe6eeaad13c928ac7d11b3f8da65b0
darknight <illuminating.me@gmail.com>
2020-02-15 20:01:40 +0700

    redo p33 of leetcode, update template

--- leetcode/_template.py
+++ leetcode/_template.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-import math, itertools
+import math, itertools, functools
 from collections import defaultdict, Counter
-from typing import List, Set
+from typing import List, Set, Dict, Tuple
 try:
     from _tree import *
     from _list import *
--- leetcode/p0033-search-in-rotated-sorted-array.py
+++ leetcode/p0033-search-in-rotated-sorted-array.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
+from typing import List
+
 class Solution:
     # @param {integer[]} nums
     # @param {integer} target
     # @return {integer}
-    def search(self, nums, target):
+    def AC_search(self, nums, target):
         
         if len(nums) < 1:
             return -1
@@ -34,8 +36,62 @@ class Solution:
                     i -= 1
         return index
 
+    def search(self, nums: List[int], target: int) -> int:
+        size = len(nums)
+        if size == 0:
+            return -1
+        if size <= 1:
+            if nums[0] == target:
+                return 0
+            return -1
+
+        low = nums[0]
+        high = nums[-1]
+        if target == low:
+            return 0
+        if target == high:
+            return size - 1
+        if high < target < low:
+            return -1
+
+        i = 0
+        j = size - 1
+        while i <= j:
+            k = i + (j - i) // 2
+            if target == nums[k]:
+                return k
+            if target > low:
+                if nums[k] < low:
+                    j = k - 1
+                elif target < nums[k]:
+                    j = k - 1
+                else:
+                    i = k + 1
+            if target < high:
+                if nums[k] > high:
+                    i = k + 1
+                elif target < nums[k]:
+                    j = k - 1
+                else:
+                    i = k + 1
+
+        return -1
+
 
 if __name__ == '__main__':
-    print(Solution().search([4,5,6,0,1,2,3], 2))
-    print(Solution().search([4,5,6,0,1,2,3], 6))
-    print(Solution().search([1], 0))
+    # print(Solution().search([4,5,6,0,1,2,3], 2))
+    # print(Solution().search([4,5,6,0,1,2,3], 6))
+    # print(Solution().search([1], 0))
+    assert Solution().search([], 5) == -1
+    assert Solution().search(nums = [4,5,6,7,0,1,2], target = 3) == -1
+    assert Solution().search(nums = [4,5,6,7,0,1,2], target = 4) == 0
+    assert Solution().search(nums = [4,5,6,7,0,1,2], target = 7) == 3
+    assert Solution().search(nums = [4,5,6,7,0,1,2], target = 0) == 4
+    assert Solution().search(nums = [4,5,6,7,0,1,2], target = 1) == 5
+    assert Solution().search(nums = [4,5,6,7,0,1,2], target = 2) == 6
+
+    assert Solution().search(nums = [0,1,2,4,5,6,7], target = 0) == 0
+    assert Solution().search(nums = [0,1,2,4,5,6,7], target = 1) == 1
+    assert Solution().search(nums = [0,1,2,4,5,6,7], target = 3) == -1
+    assert Solution().search(nums = [0,1,2,4,5,6,7], target = 6) == 5
+    assert Solution().search(nums = [0,1,2,4,5,6,7], target = 7) == 6
